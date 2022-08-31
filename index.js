7fdb35284bd41ca4a88e81b20a15fc4b5bd800fb
mohamed-sherif662002 <110036738+mohamed-sherif662002@users.noreply.github.com>
2022-08-31 15:17:54 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+/*
+let container_js = document.getElementById("container");
+let time = 500;
+window.onload = function(){
+    let Fun_Set = setInterval(function(){
+        container_js.style.display = "block";
+        function stop(){
+            time = time + 500;
+            if(time === 4000){
+                container_js.style.display = "none";
+                clearInterval(Fun_Set);
+            }
+        }
+        stop();
+    },time);
+}  
+*/
